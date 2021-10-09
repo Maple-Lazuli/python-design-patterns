ac2325ed6bdcea57d962afbd64f7a974a6635e1d
Maple-Lazuli <ada.lazuli@spaceforce.mil>
2021-10-09 18:54:28 -0400

    added builder and factory patterns

---
+++ src/SOLID/srp.py
@@ -0,0 +1,50 @@
+# A really bad anti design pattern is throwing everything under the kitchensink into a single class
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.entries.append(f"{self.count}: {text}")
+        self.count += 1
+
+    def remove_entry(self, pos):
+        del self.entries[pos]
+
+    def __str__(self):
+        return "\n".join(self.entries)
+
+    # break SRP
+    # This breaks SRP by taking on the responsibility of persistence
+
+    def save(self, filename):
+        file = open(filename, "w")
+        file.write(str(self))
+        file.close()
+
+    def load(self, filename):
+        pass
+
+    def load_from_web(self, uri):
+        pass
+
+# This is more in line with SRP because this class has a single responsibility
+class PersistenceManager:
+    def save_to_file(journal, filename):
+        file = open(filename, "w")
+        file.write(str(journal))
+        file.close()
+
+
+j = Journal()
+j.add_entry("I cried today.")
+j.add_entry("I ate a bug.")
+print(f"Journal entries:\n{j}\n")
+
+p = PersistenceManager()
+file = r'c:\temp\journal.txt'
+p.save_to_file(j, file)
+
+# verify!
+with open(file) as fh:
+    print(fh.read())
